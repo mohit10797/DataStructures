2f4cbc2651bc89f0c5da8f315d677b98b61abb95
mohit guglani <mohitguglani73@gmail.com>
2023-05-18 01:53:51 +0530

    added two new questions for stack

---
+++ EvaluateReversePolishNotation.cs
@@ -0,0 +1,92 @@
+﻿public class Solution
+{
+
+    int num = 0;
+    int num2 = 0;
+
+    public int EvalRPN(string[] tokens)
+    {
+        MinStack stack = new MinStack();
+        foreach (var token in tokens)
+        {
+
+            switch (token)
+            {
+                case "+":
+                    this.num = stack.top.value;
+                    this.num2 = stack.top.next.value;
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Push(this.num + this.num2);
+                    break;
+                case "/":
+                    num = stack.top.value;
+                    num2 = stack.top.next.value;
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Push(num2 / num);
+                    break;
+                case "*":
+                    this.num = stack.top.value;
+                    this.num2 = stack.top.next.value;
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Push(num * num2);
+                    break;
+                case "-":
+                    this.num = stack.top.value;
+                    this.num2 = stack.top.next.value;
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Push(num2 - num);
+                    break;
+                default:
+                    stack.Push(Convert.ToInt32(token));
+                    break;
+            }
+        }
+        return stack.top.value;
+
+    }
+}
+
+public class MinStack
+{
+
+    public node top;
+
+
+    public void Push(int val)
+    {
+        if (this.top != null)
+        {
+            node n = new node(val);
+            var temp = this.top;
+            this.top = n;
+            this.top.next = temp;
+        }
+        else
+        {
+            this.top = new node(val);
+            this.top.next = null;
+        }
+    }
+
+    public void Pop()
+    {
+        this.top = this.top.next;
+
+    }
+
+}
+
+public class node
+{
+    public int value;
+    public node next;
+    public node(int val)
+    {
+        this.value = val;
+        this.next = null;
+    }
+}
---
+++ MinimumStack.cs
@@ -0,0 +1,56 @@
+﻿public class MinStack
+{
+
+    public node top;
+
+    public List<int> min = new List<int>();
+    public MinStack()
+    {
+
+    }
+
+    public void Push(int val)
+    {
+        if (this.top != null)
+        {
+            node n = new node(val);
+            var temp = this.top;
+            this.top = n;
+            this.top.next = temp;
+            min.Add(Math.Min(val, min[min.Count - 1]));
+        }
+        else
+        {
+            this.top = new node(val);
+            this.top.next = null;
+            min.Add(val);
+        }
+    }
+
+    public void Pop()
+    {
+        this.top = this.top.next;
+
+    }
+
+    public int Top()
+    {
+        return top.value;
+    }
+
+    public int GetMin()
+    {
+        return min[min.Count - 1];
+    }
+}
+
+public class node
+{
+    public int value;
+    public node next;
+    public node(int val)
+    {
+        this.value = val;
+        this.next = null;
+    }
+}
